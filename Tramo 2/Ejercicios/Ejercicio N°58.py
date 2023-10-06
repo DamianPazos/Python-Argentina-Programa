ea32168347b0d5cef0f376bb402303a5fabe732d
Damian Pazos <damian_ariel_pazos>
2023-10-05 22:09:23 -0300

    Resolucion de ejercicios N°58 y N°59

--- Tramo 2/Ejercicios/Ejercicio N°58.py
+++ Tramo 2/Ejercicios/Ejercicio N°58.py
@@ -11,3 +11,36 @@ a- Cantidad de ventas realizadas total.
 b- Cantidad de ventas que se aplicaron un 10% de descuento.
 c- Cantidad de ventas que SOLO se aplicó el precio base, no se le realizaron descuentos
 '''
+
+check = True
+list_product = []
+counter_all_sales = 0
+counter_discount_sales = 0
+counter_base_price_sales = 0
+while check:
+    product = {
+        'price' : None,
+        'quantity' : None
+    }
+    quantity = int(input('Ingrese la canitdad de unidades del producto: '))
+    if quantity != -1 and quantity > 0:
+        price = round(float(input('Ingrese el precio base del producto: $')), 2)
+        if quantity > 12 and quantity <= 100:
+            price = price - price * 0.1
+            counter_discount_sales += 1
+            counter_all_sales += 1
+        elif quantity > 100:
+            price = price - price * 0.25
+            counter_all_sales += 1
+        else:
+            counter_all_sales += 1
+            counter_base_price_sales += 1
+        product['quantity'] = quantity
+        product['price'] = price
+        list_product.append(product)
+        print(f'El precio total es ${product["price"] * product["quantity"]} y su precio por unidad es {product["price"]}')
+    else:
+        check = False
+print(f'Cantidad de ventas: {counter_all_sales}')
+print(f'Cantidad de ventas con 10% de descuento: {counter_discount_sales}')
+print(f'Cantidad de ventas sin descuento: {counter_base_price_sales}')
--- Tramo 2/Ejercicios/Ejercicio N°59.py
+++ Tramo 2/Ejercicios/Ejercicio N°59.py
@@ -13,3 +13,31 @@ Se debe ingresar una F para finalizar el día de venta y arrojar los siguientes 
 | Total de Venta   | $ xxxx.xx |
 | Importe del IVA  | $ xxxx.xx |
 '''
+
+cash = 0
+check = 0
+credit_card = 0
+flag = True
+while flag:
+    amount_received = round(float(input('Ingrese el importe cobrado: $')), 2)
+    payment_method = input('Ingrese el metodo de pago: ')
+    if payment_method == 'C' or payment_method == 'c':
+        amount_received = amount_received + amount_received * 0.2
+        check = check + amount_received
+    elif payment_method == 'E' or payment_method =='e':
+        amount_received = amount_received - amount_received * 0.1
+        cash = cash + amount_received
+    elif payment_method == 'T' or payment_method == 't':
+        amount_received = amount_received + amount_received * 0.12
+        credit_card = credit_card + amount_received
+    elif payment_method == 'F' or payment_method == 'f':
+        flag = False
+    else:
+        print('Ingreso una opcion incorrecta, vuelva a ingresar los datos')
+print('| Forma de Pago    | Total     ')
+print('| ---------------- | --------- ')
+print(f'| Efectivo en Caja | $ {cash} ')
+print(f'| Tarjeta Crédito  | $ {credit_card} ')
+print(f'| Cheques          | $ {check} ')
+print(f'| Total de Venta   | $ {cash + credit_card + check} ')
+print(f'| Importe del IVA  | $ {round((cash + credit_card + check) * 0.21, 2)} ')

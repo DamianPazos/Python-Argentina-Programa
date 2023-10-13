c7a21f73dc3222aa945e68d3f2496d6759e35d35
Damian Pazos <damian_ariel_pazos>
2023-10-13 19:55:18 -0300

    Resolucion de ejercicios N°61 y N°62

--- Tramo 2/Ejercicios/Ejercicio N°61.py
+++ Tramo 2/Ejercicios/Ejercicio N°61.py
@@ -2,3 +2,11 @@
 Escribir un programa que permita ingresar un número entero positivo y mostrar su factorial. 
 El factorial de un número es el producto de todos los números enteros desde 1 hasta el número ingresado. 
 '''
+
+def factorial(nro):
+    if nro == 0:
+        return 1
+    else:
+        return factorial(nro - 1)* nro
+number = int(input('Ingrese un numero entero positivo: '))
+print(factorial(number))
--- Tramo 2/Ejercicios/Ejercicio N°62.py
+++ Tramo 2/Ejercicios/Ejercicio N°62.py
@@ -11,3 +11,42 @@ Se pide:
 Diseñar un algoritmo para registrar los datos y decidir si es apto para la competencia. 
 *Sólo en caso de estar apto, informar el promedio y el número de día en el que realizó el menor tiempo
 '''
+
+def control_time(quantity):
+    list_time = []
+    for x in range(quantity):
+        time = int(input('Ingrese el tiempo: '))
+        list_time.append(time)
+    return list_time
+def average(list):
+    sum_time = 0
+    for x in list:
+        sum_time = sum_time + x
+    return sum_time / len(list)
+def min_time(list):
+    time = None
+    for x in list:
+        if time == None:
+            time = x
+        elif time > x:
+            time = x
+    return time
+def apt(list):
+    flag = False
+    for x in list:
+        if x > 20:
+            return False
+        elif x < 15:
+            flag = True
+    if flag == True and average(list) <= 18:
+        return True
+    else: 
+        return False
+list_time_athlete = control_time(10)
+is_apt = apt(list_time_athlete)
+if is_apt == True:
+    print('Es apto')
+    print(f'Menor tiempo {min_time(list_time_athlete)} minutos')
+    print(f'Tiempo promedio {average(list_time_athlete)}')
+else:
+    print('No es apto')
